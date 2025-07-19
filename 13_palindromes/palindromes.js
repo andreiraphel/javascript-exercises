f7ce60bf7f86638d0e1ec5b812ddfa2894586182
andreiraphel <andreiraphel@gmail.com>
2025-07-19 11:47:55 +0800

    finished js exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let fixed_word = (word.replace(/[^a-zA-Z0-9]/g, '')).toLowerCase();
+    let word_len = fixed_word.length - 1;
+    for (let i = 0; i <= word_len/2; i++){
+        if(fixed_word[i] != fixed_word[word_len-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest_age = 0;
+    let oldest_name = "";
+    for(let i = 0; i < array.length; i++){
+        if(!array[i].yearOfDeath){
+            let d = new Date();
+            array[i].yearOfDeath = d.getFullYear();
+        }
+        let age = array[i].yearOfDeath - array[i].yearOfBirth;
 
+        if(age > oldest_age){
+            oldest_name = array[i].name;
+            oldest_age = age;
+        }
+    }
+    return array.find(person => person.name === oldest_name);
 };
 
 // Do not edit below this line
